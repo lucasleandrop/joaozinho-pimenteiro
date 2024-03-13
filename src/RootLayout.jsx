6c8feab9cf3595f8a4988cf236e2e978994df5f8
José Augusto da França <j.augustodafranca@gmail.com>
2024-03-13 11:51:13 -0300

    chore: Example how to handle menu

--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -1,16 +1,73 @@
+import { useState, useEffect } from "react";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 import AsideBar from "./components/AsideBar";
 
 export default function RootLayout() {
+  const [displayMenu, setDisplayMenu] = useState(true);
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const handleWindowSizeChange = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  const isMobile = width <= 480;
+
+  const renderContent = () => {
+    if (!isMobile) {
+      return (
+        <>
+          <Header />
+          <main>
+            <AsideBar />
+            <Outlet />
+          </main>
+        </>
+      );
+    }
+
+    return (
+      <main>
+        {displayMenu ? (
+          <>
+            <Header />
+            <AsideBar />
+          </>
+        ) : (
+          <Outlet />
+        )}
+      </main>
+    );
+  };
+
   return (
     <div>
-      <Header />
-      <main>
-        <AsideBar />
-        <Outlet />
-      </main>
+      {isMobile ? (
+        <button
+          type="button"
+          style={{
+            position: "absolute",
+            top: 10,
+            right: 10,
+            zIndex: 1,
+          }}
+          onClick={() => {
+            setDisplayMenu((prevState) => !prevState);
+          }}
+        >
+          MENU
+        </button>
+      ) : null}
+      {renderContent()}
       <Footer />
     </div>
   );
